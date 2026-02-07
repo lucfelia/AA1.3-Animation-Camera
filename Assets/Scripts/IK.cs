8adbcd4496d17f6b2ea3fed688ea42a99c0f4136
lucfelia <lucia.puga@estudiant.enti.cat>
2026-02-07 19:09:16 +0100

    1. Animation Controllar & IK

---
+++ Assets/Scripts/IK.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class IK : MonoBehaviour
+{
+    Animator anim;
+    public float lookWeight = 1.0f;
+    public Transform look;
+    public float rightHandWeight = 1.0f;
+    public Transform rightHand;
+    public float leftHandWeight = 1.0f;
+    public Transform leftHand;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+        Debug.Log(anim);
+    }
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        //Cabeza look
+        anim.SetLookAtWeight(lookWeight, lookWeight);
+        anim.SetLookAtPosition(look.position);
+
+        //Right Hand
+        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, rightHandWeight);
+        anim.SetIKRotationWeight(AvatarIKGoal.RightHand, rightHandWeight);
+        anim.SetIKPosition(AvatarIKGoal.RightHand, rightHand.position);
+        anim.SetIKRotation(AvatarIKGoal.RightHand, rightHand.rotation);
+
+        //Left Hand
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, leftHandWeight);
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, leftHandWeight);
+        anim.SetIKPosition(AvatarIKGoal.LeftHand, leftHand.position);
+        anim.SetIKRotation(AvatarIKGoal.LeftHand, leftHand.rotation);
+    }
+
+}
